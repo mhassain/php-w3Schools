8040e9ef7300636bb5f82f4e94ccffccb9e6f6f4
Mahade Hasan <mahadehasan88@gmail.com>
2021-05-20 22:31:01 +0600

    php-form-validation

--- form_validate.php
+++ form_validate.php
@@ -5,17 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form-Validate</title>
+    <style>
+        .error{
+            color: red;
+        }
+    </style>
 </head>
 <body>
     <?php
-        $name = $email = $website = $comment = $gender = " ";
+        $nameErr = $emailErr = $websiteErr = $genderErr = "";
+        $name = $email = $website = $comment = $gender = "";
 
         if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $name = test_input($_POST["name"]);
-            $email = test_input($_POST["email"]);
-            $website = test_input($_POST["website"]);
-            $comment = test_input($_POST["comment"]);
-            $gender = test_input($_POST["gender"]);
+            if(empty($_POST['name'])){
+                $nameErr = "Name is required";
+            }else{
+                $name = test_input($_POST["name"]);
+            }
+            
+            if(empty($_POST['email'])){
+                $emailErr = "Email is required";
+            }else{
+                $email = test_input($_POST["email"]);
+            }
+            
+            if(empty($_POST['website'])){
+                $websiteErr = "Website is required";
+            }else{
+                $website = test_input($_POST["website"]);
+            }
+            
+            if(empty($_POST['comment'])){
+                $comment = "";
+            }else{
+                $comment = test_input($_POST["comment"]);
+            }
+
+            if(empty($_POST["gender"])){
+                $genderErr = "Gender is required";
+            }else{
+                $gender = test_input($_POST["gender"]);
+            }
+
         }
 
         function test_input($data){
@@ -30,10 +61,13 @@
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
         Name: <input type="text" name="name">
+        <span class="error">* <?php echo $nameErr; ?></span>
         <br><br>
         E-mail: <input type="text" name="email">
+        <span class="error">* <?php echo $emailErr; ?></span>
         <br><br>
         Website: <input type="text" name="website">
+        <span class="error">* <?php echo $websiteErr;?></span>
         <br><br>
         Comment: <textarea name="comment" rows="5" cols="40"></textarea>
         <br><br>
@@ -41,6 +75,7 @@
         <input type="radio" name="gender" value="female">Female
         <input type="radio" name="gender" value="male">Male
         <input type="radio" name="gender" value="other">Other
+        <span class="error">* <?php echo $genderErr;?></span>
         <br><br>
         <input type="submit" name="submit" value="Submit">  
     </form>
